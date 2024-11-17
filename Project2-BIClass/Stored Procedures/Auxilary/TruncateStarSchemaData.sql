7d9a98acfc87c07e2b2e4d4d44eae1586b5156cb
benjaminzhong1 <benjaminzhong1@gmail.com>
2024-11-17 18:10:18 -0500

    added draft of truncatestarschema

--- Project2-BIClass/Stored Procedures/Auxilary/TruncateStarSchemaData.sql
+++ Project2-BIClass/Stored Procedures/Auxilary/TruncateStarSchemaData.sql
@@ -17,39 +17,35 @@ CREATE PROCEDURE [Project2].[TruncateStarSchemaData]
 AS 
 BEGIN 
 	SET NOCOUNT ON
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	
-	print 'insert your statements within the Begin\End block which is the equivalentof the Java { \ }'
+	DECLARE TableCursor CURSOR FOR
+	SELECT DISTINCT 
+		'[' + TABLE_SCHEMA + '].[' + TABLE_NAME + ']' as FullyQualifiedTableName
+	FROM INFORMATION_SCHEMA.TABLES
+	WHERE TABLE_TYPE = 'Base Table' AND TABLE_SCHEMA LIKE '%CH01%'
+
+	OPEN TableCursor
+	DECLARE @TableName NVARCHAR(255)
+	DECLARE @SQL VARCHAR(MAX)
+
+	FETCH NEXT FROM TableCursor INTO @TableName
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN 
+		SET @SQL = 'TRUNCATE TABLE ' + @TableName
+		EXEC(@SQL)
+
+		FETCH NEXT FROM TableCursor INTO @TableName
+	END
+
+	CLOSE TableCursor
+
+	DEALLOCATE TableCursor
+
 END
 GO
 
-EXEC [Project2].[TruncateStarSchemaData] @UserAuthorizationKey = 1;
-GO 
+-- EXEC [Project2].[TruncateStarSchemaData] @UserAuthorizationKey = 1
 
 
-DECLARE TableCursor CURSOR FOR
-SELECT DISTINCT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_TYPE = 'Base Table' AND TABLE_SCHEMA LIKE '%CH01%'
 
-OPEN TableCursor
-DECLARE @TableName NVARCHAR(255)
-DECLARE @SQL VARCHAR(MAX)
-
-FETCH NEXT FROM TableCursor INTO @TableName
-
-WHILE @@FETCH_STATUS = 0
-BEGIN 
-	SET @SQL = 'TRUNCATE TABLE ' + @TableName
-	EXEC(@SQL)
-
-	FETCH NEXT FROM TableCursor INTO @TableName
-END
-
-CLOSE TableCursor
-
-DEALLOCATE TableCursor
-
-TRUNCATE TABLE [CH01-01-Dimension].[DimCustomer]
 
