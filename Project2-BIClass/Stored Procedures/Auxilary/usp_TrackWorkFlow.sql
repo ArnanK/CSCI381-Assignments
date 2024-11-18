038cd31e96c19e61ac1e09ecebdd63ed71ed5ed6
Luis Diaz <luludiaz1997@gmail.com>
2024-11-17 22:57:46 -0500

    Work Flow name

--- Project2-BIClass/Stored Procedures/Auxilary/usp_TrackWorkFlow.sql
+++ Project2-BIClass/Stored Procedures/Auxilary/usp_TrackWorkFlow.sql
@@ -1,3 +1,15 @@
+
+-- =============================================
+-- Author: Luis Diaz
+-- Create date: 11/16/2024
+-- Description: 
+/* 
+Stores procedure logs workflow steps into the Process.WorkFlowSteps
+ table by inserting a description, user authorization key, 
+ and the number of rows affected during a specific workflow step.
+*/
+-- =============================================
+
 CREATE OR ALTER PROCEDURE [Process].[usp_TrackWorkFlow]
     @UserAuthorizationKey INT,
     @WorkFlowStepTableRowCount INT,
