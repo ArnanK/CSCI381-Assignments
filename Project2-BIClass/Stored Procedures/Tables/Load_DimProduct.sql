a7fa74aa2ba77ff7f1b01658af31d70063cc6c61
Inderpreet Singh <54847601+Inder3428@users.noreply.github.com>
2024-11-18 15:46:18 -0500

    Update Load_DimProduct.sql
    debugged: 
    CREATE OR ALTER PROCEDURE:
    Ensures the procedure is created if it doesn't exist or modified if it does.
    Consistent Naming and Reference:
    
    Ensured all columns and table references ([FileUpload].OriginallyLoadedData, [CH01-01-Dimension].[DimProductCategory], [CH01-01-Dimension].[DimProductSubcategory]) are correctly referenced.
    @WorkFlowStepDescription:
    
    Confirmed the workflow step description matches the functionality ('Loading data into the DimProduct Table').
    Column Mapping Validation:
    
    Verified that columns from OriginallyLoadedData correctly align with DimProduct.

--- Project2-BIClass/Stored Procedures/Tables/Load_DimProduct.sql
+++ Project2-BIClass/Stored Procedures/Tables/Load_DimProduct.sql
@@ -2,16 +2,18 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
--- Author:      Nafisul Islam
+-- Author:      Nafisul Islam Debugged by: Inderpreet Singh
 -- Create date: 11/17/2024
 -- Description: Loads data into the DimProduct table.
 -- =============================================
-ALTER PROCEDURE [Project2].[Load_DimProduct]
+CREATE OR ALTER PROCEDURE [Project2].[Load_DimProduct]
     @UserAuthorizationKey INT
 AS
 BEGIN
     SET NOCOUNT ON;
+
     DECLARE @WorkFlowStepTableRowCount INT; -- Declaring the variable
 
     -- Insert data into DimProduct table
@@ -39,19 +41,25 @@ BEGIN
             dps.ProductSubcategoryKey,
             dpc.ProductCategory,
             dps.ProductSubcategory,
-            ProductCode,
-            ProductName,
-            Color,
-            ModelName
+            old.ProductCode,
+            old.ProductName,
+            old.Color,
+            old.ModelName
         FROM [FileUpload].OriginallyLoadedData AS old
-        INNER JOIN [CH01-01-Dimension].[DimProductCategory] AS dpc ON old.ProductCategory = dpc.ProductCategory
-        INNER JOIN [CH01-01-Dimension].[DimProductSubcategory] as dps ON old.ProductSubcategory = dps.ProductSubcategory
+        INNER JOIN [CH01-01-Dimension].[DimProductCategory] AS dpc 
+            ON old.ProductCategory = dpc.ProductCategory
+        INNER JOIN [CH01-01-Dimension].[DimProductSubcategory] AS dps 
+            ON old.ProductSubcategory = dps.ProductSubcategory
     ) AS new;
+
+    -- Record the number of rows inserted
     SELECT @WorkFlowStepTableRowCount = @@ROWCOUNT;
+
+    -- Track workflow for the operation
     EXEC [Process].[usp_TrackWorkFlow] 
         @WorkFlowStepDescription = 'Loading data into the DimProduct Table',
         @UserAuthorizationKey = @UserAuthorizationKey, 
-        @WorkFlowStepTableRowCount = @WorkFlowStepTableRowCount; -- Use the variable in EXEC statement
+        @WorkFlowStepTableRowCount = @WorkFlowStepTableRowCount;
 
 END
 GO
