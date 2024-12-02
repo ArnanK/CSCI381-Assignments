972e1d10f3321ef500c3311845ec027ccbc080cb
dyllp <dyllpic@gmail.com>
2024-12-02 15:18:26 -0500

    Created stored procedures for loading Mode Of Instruction and Instructor tables.

---
+++ Project3-QueensCollegeSchedule/Stored Procedures/load-data/LoadInstructor.sql
@@ -0,0 +1,34 @@
+-- =============================================
+-- Author: Dillon Chen
+-- Create date: 12/2/2024
+-- Description: Load Instructor Stored Procedure
+-- =============================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP PROCEDURE IF EXISTS [Project3].[LoadInstructor]
+GO
+
+CREATE PROCEDURE [Project3].[LoadInstructor]
+	@UserAuthorizationKey INT
+AS 
+BEGIN 
+	
+	TRUNCATE TABLE [Project3].[Instructor]
+
+	INSERT INTO [Project3].[Instructor]
+	(InstructorName)
+		(   
+			SELECT DISTINCT
+				C.Instructor AS InstructorName
+			FROM Uploadfile.CurrentSemesterCourseOfferings AS C
+		)
+	-- EXEC workflowsteps procedure
+END
+GO
+
+-- EXEC [LoadData].[LoadInstructor] @UserAuthorizationKey = 1
+-- SELECT *
+-- FROM [Project3].[Instructor]
---
+++ Project3-QueensCollegeSchedule/Stored Procedures/load-data/LoadModeOfInstruction.sql
@@ -0,0 +1,35 @@
+-- =============================================
+-- Author: Dillon Chen
+-- Create date: 12/2/2024
+-- Description: Load Mode Of Instruction Stored Procedure
+-- =============================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP PROCEDURE IF EXISTS [Project3].[LoadModeOfInstruction]
+GO
+
+CREATE PROCEDURE [Project3].[LoadModeOfInstruction]
+	@UserAuthorizationKey INT
+AS 
+BEGIN 
+	
+	TRUNCATE TABLE [Project3].[ModeOfInstruction]
+
+	INSERT INTO [Project3].[ModeOfInstruction]
+	(ModeName, UserAuthorizationKey)
+		(   
+			SELECT DISTINCT
+				C.ModeOfInstruction AS ModeName,
+				@UserAuthorizationKey
+			FROM Uploadfile.CurrentSemesterCourseOfferings AS C
+		)
+	-- EXEC workflowsteps procedure
+END
+GO
+
+-- EXEC [LoadData].[LoadModeOfInstruction] @UserAuthorizationKey = 1
+-- SELECT *
+-- FROM [Project3].[ModeOfInstruction]
