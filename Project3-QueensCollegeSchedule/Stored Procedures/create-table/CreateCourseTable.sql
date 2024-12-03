9798d9081e951dfbf4852471152af8e7bb9b036b
dyllp <dyllpic@gmail.com>
2024-12-03 16:59:49 -0500

    Course Table Create and Load Procedures.

---
+++ Project3-QueensCollegeSchedule/Stored Procedures/create-table/CreateCourseTable.sql
@@ -0,0 +1,39 @@
+-- =============================================
+-- Author: Dillon Chen
+-- Create date: 12/3/2024
+-- Description: Create Course Table Stored Procedure
+-- =============================================
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP PROCEDURE IF EXISTS [Project3].[CreateCourseTable]
+GO
+
+CREATE PROCEDURE [Project3].[CreateCourseTable]
+	@UserAuthorizationKey INT
+AS 
+BEGIN
+	DROP TABLE IF EXISTS [Project3].[Course]
+	CREATE TABLE [Project3].[Course]
+		(
+			CourseID [Udt].[P3Key] NOT NULL CONSTRAINT [DF_Course] DEFAULT (NEXT VALUE FOR [Project3].CourseID_Seq) PRIMARY KEY,
+			CourseName [Udt].[P3NameString] NULL,
+			CourseNum [Udt].[P3NameString] NULL,
+            Semester [Udt].[P3NameString] NULL,
+            DepartmentID INT NULL,
+            Credits DECIMAL(5,2) NULL,
+            Hours DECIMAL(5,2) NULL,
+            WritingIntensive [Udt].[P3Bool] NULL,
+			UserAuthorizationKey INT NULL
+		)
+
+	-- Track workflow for the operation
+    EXEC [Process].[usp_TrackWorkFlow]
+        @WorkFlowStepDescription = 'Create Course Table',
+        @UserAuthorizationKey = @UserAuthorizationKey,
+        @WorkFlowStepTableRowCount = -1;
+END
+GO
---
+++ Project3-QueensCollegeSchedule/Stored Procedures/load-data/LoadCourseTable.sql
@@ -0,0 +1,44 @@
+-- =============================================
+-- Author: Dillon Chen
+-- Create date: 12/3/2024
+-- Description: Load Course Stored Procedure
+-- =============================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP PROCEDURE IF EXISTS [Project3].[LoadCourse]
+GO
+
+CREATE PROCEDURE [Project3].[LoadCourse]
+	@UserAuthorizationKey INT
+AS 
+BEGIN 
+	DECLARE @WorkFlowStepTableRowCount INT;
+	INSERT INTO [Project3].[Course]
+	(CourseName, CourseNum, SemesterID, DepartmentID, Credits, Hours, WritingIntensive, UserAuthorizationKey)
+		(   
+			SELECT DISTINCT
+				SUBSTRING(C1.[Course (hr, crd)], 0, CHARINDEX(' ', C1.[Course (hr, crd)])),
+                SUBSTRING(C1.[Course (hr, crd)], CHARINDEX(' ', C1.[Course (hr, crd)]), CHARINDEX('(', C1.[Course (hr, crd)]) - CHARINDEX(' ', C1.[Course (hr, crd)])-1),
+                C1.Semester,
+                1234, --Replace with DepartmentID
+                CAST(SUBSTRING(C1.[Course (hr, crd)], CHARINDEX('(', C1.[Course (hr, crd)])+1, CHARINDEX(',', C1.[Course (hr, crd)]) - CHARINDEX('(', C1.[Course (hr, crd)])-1) AS DECIMAL(5,2)),
+                CAST(SUBSTRING(C1.[Course (hr, crd)], CHARINDEX(',', C1.[Course (hr, crd)])+1, CHARINDEX(')', C1.[Course (hr, crd)]) - CHARINDEX(',', C1.[Course (hr, crd)])-1) AS DECIMAL(5,2)),
+                (CASE
+                    WHEN C1.[Course (hr, crd)] LIKE N'%[0-9]%W%' THEN N'YES'
+                    ELSE N'NO'
+                 END),
+				@UserAuthorizationKey
+			FROM Uploadfile.CurrentSemesterCourseOfferings AS C1
+		)
+	SELECT @WorkFlowStepTableRowCount = @@ROWCOUNT;
+		-- Track workflow for the operation
+    EXEC [Process].[usp_TrackWorkFlow]
+        @WorkFlowStepDescription = 'Load Course Data',
+        @UserAuthorizationKey = @UserAuthorizationKey,
+        @WorkFlowStepTableRowCount = @WorkFlowStepTableRowCount;
+END
+GO
+
